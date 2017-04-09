d866ee3f95f0141fe8e0cddf2e79de90c413d136
Swapnil Walke <iamlinonymous@gmail.com>
2017-04-09 18:19:28 +0530

    - modified checks - Hybrid Works Now

--- Recommender/HybridCalculation.py
+++ Recommender/HybridCalculation.py
@@ -16,30 +16,69 @@ def read_user_item():
         data = json.load(data_file)
     return data
 
+def write_final(d):
+    with open('rating.json', 'w') as f:
+        json.dump(d, f)
+        
 def hybrid(a, b):
     """
         hybrid calculation of ratings
     """
     c = {}
-    alpha = 1/5
-    beta = 5/6
+    alpha = 1/float(6)
+    beta = 5/float(6)
+    i = 0
+    cnt = 0
+    #print a.keys()
     for user in a:
+        i += 1
         for item in b:
+            #print user
+            #print item
             if user not in c.keys():
                 c[user] = []
-            if item in b.keys() and user in b[item]:
-                u_i = b[item][user]
-                u_u = a[user][item]
-            elif item in b.keys() and user not in b[item]:
+            if item in b.keys() and item in a[user][0].keys() and user in b[item][0].keys():
+                u_i = b[item][0][user]
+                u_u = a[user][0][item]
+                cnt += 1
+            elif item in b.keys() and user not in b[item][0].keys() and item in a[user][0].keys():
                 u_i = 0
-                u_u = a[user][item]
-            elif item not in b.keys():
+                u_u = a[user][0][item]
+            elif item not in b.keys() or item not in a[user][0].keys():
                 continue
+            #rating = max(u_u , u_i)
+            #print 1/float(6) * u_u
+            #print u_i
+            #print 5/float(6) * u_i
+            #print 
             rating = alpha *  u_u + beta * u_i
+            #print rating
+            #break
             c[user].append({item : rating})
-        
+        print i
+        print "cnt " + str(cnt)
+        #break
+        if i % 15 == 0:
+            write_final(c)
         
 def hybrid_runner():
-    user = read_user_user()
-    item = read_user_item()
-    hybrid(user, item)
+    users = read_user_user()
+    items = read_user_item()
+    #print users["1"][0]["Iron Giant, The (1999)"]
+    #print items["Iron Giant, The (1999)"][0]["1"]
+    """
+    d = {}
+    #print users['1'][0]
+    for user in users:
+        if user not in d.keys():
+            d[user] = {}
+        for item in users[user][0]:
+            print item
+            print users[user][0][item]
+            break
+        break
+    #print user['1']['Little Big League (1994)']
+    #print item['Iron Giant, The (1999)']"""
+    hybrid(users, items)
+    
+hybrid_runner()
